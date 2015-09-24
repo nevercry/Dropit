b8cddcb3bfecbabb3cef6688d4154ce0cbd2c54b
nevercry <ysfnevercry@gmail.com>
2015-09-24 22:15:05 +0800

    learn swift

---
+++ Dropit/BezierPathsView.swift
@@ -0,0 +1,25 @@
+//
+//  BezierPathsView.swift
+//  Dropit
+//
+//  Created by nevercry on 9/24/15.
+//  Copyright © 2015 nevercry. All rights reserved.
+//
+
+import UIKit
+
+class BezierPathsView: UIView {
+
+    private var bezierPaths = [String:UIBezierPath]()
+    
+    func setPath(path: UIBezierPath?, named name:String) {
+        bezierPaths[name] = path
+        setNeedsDisplay()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        for (_, path) in bezierPaths {
+            path.stroke()
+        }
+    }
+}
